33c97f8c1e915e77adf9709fb21270d5b7ef48a0
Михаил <lehmanmihail1999@gmail.com>
2026-03-25 10:34:14 +0300

    lesson 4 upd

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,12 +1,2 @@
 const array = [-3,-1, 0, 3.14, 10, 20 ]
-
-const newArray = array.filter(function(element) {
-    if (element >= 0) {
-        return true
-    }
-    else {
-        return false
-    }
-})
-console.log(array)
-console.log(newArray)
+console.log(array.sort())
