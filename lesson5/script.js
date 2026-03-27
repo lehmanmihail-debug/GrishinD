85e9ff7b3e01b3b292eed3d42cbe3bc32b18b616
Михаил <lehmanmihail1999@gmail.com>
2026-03-27 15:44:14 +0300

    lesson 5

---
+++ lesson5/script.js
@@ -0,0 +1,8 @@
+const clicker = () => {
+const first = document.querySelector("div")
+first.textContent = "TEST"
+
+}
+
+const mybtn =document.querySelector(".mybtn")
+mybtn.addEventListener("click", clicker)
