573a83d9568e72acea99f160c74ba4b36250a832
Михаил <lehmanmihail1999@gmail.com>
2026-04-20 21:26:26 +0300

    car

--- carousel/script.js
+++ carousel/script.js
@@ -16,12 +16,12 @@ function createProducts()  {
     productOne.className = 'product';
 
     productTwo.innerHTML = `
-    <img class="product" src='https://avatars.mds.yandex.net/i?id=f50e467fee02dce6ebac8d40a09a0ded_sr-9729437-images-thumbs&n=13' alt="товар">
+    <img class="product" src='https://tse1.mm.bing.net/th/id/OIP.hxOX9S5tPaLSyZe68YHzUAHaFv?cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=31' alt="товар">
     <p>Товар 2</p>`;
     productTwo.className = 'product';
 
     productThree.innerHTML = `
-    <img class="product" src='https://media.istockphoto.com/id/2148107100/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B8%D0%B7%D0%BE%D0%BB%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D1%8D%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B3%D0%BE-%D1%87%D0%B0%D0%B9%D0%BD%D0%B8%D0%BA%D0%B0-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC-%D1%84%D0%BE%D0%BD%D0%B5.jpg?s=612x612&w=0&k=20&c=w5my4you7r5CksvheLlWEvhGYPxG1dv-rFg4Qo-8AZg=' alt="товар">
+    <img class="product" src='https://tse2.mm.bing.net/th/id/OIP.leMJVofvrmRD4VTppC20SAHaHa?cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3' alt="товар">
     <p>Товар 3</p>`;
     productThree.className = 'product';
 
