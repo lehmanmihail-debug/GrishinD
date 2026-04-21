adff0dfd9a6cf888203cec5d780f4d1debee5405
Михаил <lehmanmihail1999@gmail.com>
2026-04-22 00:20:00 +0300

    table task

--- Table/script.js
+++ Table/script.js
@@ -6,8 +6,10 @@ const users = [
 
 function fillTable (data) {
     const thead = document.getElementById("thead")
+    const tbody = document.getElementById("tbody")
     const tr = document.createElement("tr")
     const headers = Object.keys(data[0]) // выбираем ключи из 0 обьекта
+
     headers.forEach(header => {
         const td = document.createElement("td")
         tr.appendChild(td)
@@ -15,6 +17,18 @@ function fillTable (data) {
     });
 
     thead.appendChild(tr)
+
+    data.forEach(name => {
+        const tbodytd = document.createElement("tr");
+        
+        headers.forEach(header => {
+            const td = document.createElement("td");
+            td.textContent = name[header];
+            tbodytd.appendChild(td);
+        });
+        
+        tbody.appendChild(tbodytd);
+    });
 }
 
 fillTable(users)
